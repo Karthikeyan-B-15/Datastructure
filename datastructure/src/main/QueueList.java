fb49cb4911db20e8a312e4fc919f28d6b83b009f
Karthikeyan <96740322+Teslakarthi@users.noreply.github.com>
2022-04-21 16:17:00 +0530

    Add files via upload

---
+++ datastructure/src/main/QueueList.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class Queue {
+	class Node{
+		int data;
+		Node next;
+		
+		Node(int val){
+			data = val;
+			next = null;
+		}
+	}
+	
+	Node front,rear; 
+	
+	Queue(){
+		front = null;
+		rear = null;
+	}
+	public void enqueue(int val) { //O(1)
+		Node newNode = new Node(val);
+		if(front==null)
+			front = newNode;
+		else
+			rear.next = newNode	;
+		rear = newNode;
+		
+	}
+	
+	public int dequeue() {
+		if(front==null) //no node in Q
+			throw new IndexOutOfBoundsException("Queue is Empty");
+		int temp = front.data;
+		front = front.next;
+		if(front==null) //deleted last node and now Q is empty
+			rear=null;
+		return temp;
+		
+	}
+}
+public class QueueList {
+	public static void main(String args[]) {
+		Queue li=new Queue();
+		Scanner inp=new Scanner(System.in);
+		while(true) {
+		System.out.println("1.Enqueue");
+		System.out.println("2.Deque");
+		int choice=inp.nextInt();
+		switch(choice) {
+		case 1:System.out.print("Enter your data:");
+		   int val=inp.nextInt();
+		   li.enqueue(val);
+		   break;
+		case 2:System.out.println(li.dequeue() + " dequeue from Queue");
+		       break;
+		default:
+			System.out.println("Wrong Choice!!!");
+		}
+		}
+}
+}
